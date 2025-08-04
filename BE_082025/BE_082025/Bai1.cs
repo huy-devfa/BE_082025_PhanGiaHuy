b8b9e5f8cda9eacc486fd527a695d947a8602e4a
Admin <huyphangia412@gmail.com>
2025-08-04 20:35:37 +0700

    commit lan 4

---
+++ BE_082025/BE_082025/Bai1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_082025
+{
+    public class Bai1
+    {
+        /// <summary>
+        /// Hàm này để chào user khi chương trình bắt đầu
+        /// </summary>
+        /// <param name="tenUser">Lấy userName của user</param>
+        /// <param name="rankUser">lấy rank của user</param>
+        public void loiChaoBatDauChuongTrinh(string tenUser, string rankUser)
+        {
+            Console.WriteLine("Xin chao [{0}] {1}!", rankUser, tenUser);
+        }
+
+        public void tongCuaHaiSo(int sothu1, int sothu2)
+        {
+            Console.Write($"Tong cua 2 so la {sothu1 + sothu2} ");
+        }
+    }
+}
--- BE_082025/BE_082025/Program.cs
+++ BE_082025/BE_082025/Program.cs
@@ -10,7 +10,37 @@ namespace BE_082025
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Hello World");
+            Bai1 bai1 = new Bai1();
+            bai1.loiChaoBatDauChuongTrinh("Phan Gia Huy", "VIP");
+
+            Console.WriteLine("Nhap so thu nhat : ");
+            var inputData = Console.ReadLine();
+            var isNumberic = int.TryParse(inputData, out int n);
+            if (inputData == null || isNumberic == false)
+            {
+                Console.WriteLine("Vui long nhap so nguyen!");
+            }
+            else
+            {
+                var sothunhat = int.Parse(inputData);
+                Console.WriteLine("Nhap so thu hai :");
+                inputData = Console.ReadLine();
+                isNumberic = int.TryParse(inputData, out int m);
+                if (inputData == null || isNumberic == false)
+                {
+                    Console.WriteLine("Vui long nhap so nguyen!");
+                }
+                else
+                {
+                    var sothuhai = int.Parse(inputData);
+                    bai1.tongCuaHaiSo(sothunhat, sothuhai);
+                }
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
